766911f27a56db793bbcd4aa74fb1951e396e086
atms <atms@fe16d31a-7d20-0410-93c1-ede0537ddd7b>
2007-06-22 17:43:29 +0000

    leistung auf Jinja umgestellt

--- ebkus-3/trunk/lib/ebkus/html/leistung.py
+++ ebkus-3/trunk/lib/ebkus/html/leistung.py
@@ -2,70 +2,82 @@
 
 """Module für die Leistung."""
 
-import string
-
 from ebkus.app import Request
-from ebkus.app.ebapi import Akte, Fall, Leistung, cc, today
-from ebkus.app.ebapih import get_codes, mksel
-from ebkus.app_surface.standard_templates import *
-from ebkus.app_surface.leistung_templates import *
+from ebkus.app.ebapi import Fall, Leistung, today
+from ebkus.app.ebapih import get_codes, make_option_list
+from ebkus.config import config
 
-
-class leistneu(Request.Request):
+class _leist(Request.Request):
+    # leistneu und updleist mit derselben Routine,
+    # nur anders parametrisiert
+    def _process(self, REQUEST, RESPONSE,
+                 title,
+                 legendtext,
+                 fall,
+                 leistung,
+                 file,
+                 mitarbeiter_selected,
+                 leistung_selected,
+                 ):
+        leistungen = fall['leistungen']
+        leistungen.sort('bgy', 'bgm', 'bgd')
+        leistungsarten = get_codes('fsle')
+        # hier kommt alles rein, was das template braucht
+        context_dict = {
+            'weiterleitung': False,
+            'title': title,
+            'legendtext': legendtext,
+            'fall': fall,
+            'file': file,
+            'mitarbeiter_options': make_option_list(self.getMitarbeiterliste(),
+                                                    'id', 'na',
+                                                    selected=mitarbeiter_selected),
+            'leistungsart_options': make_option_list(leistungsarten,
+                                                    'id', 'name',
+                                                    selected=leistung_selected),
+            'leistung': leistung,
+            'leistungen': leistungen,
+            }
+        return self.render('leistung', context_dict)
+    
+class leistneu(_leist):
     """Neue Leistung eintragen. (Tabelle: Leistung.)"""
     
     permissions = Request.UPDATE_PERM
     
     def processForm(self, REQUEST, RESPONSE):
-        mitarbeiterliste = self.getMitarbeiterliste()
-        user = self.user
         if self.form.has_key('fallid'):
             fallid = self.form.get('fallid')
         else:
             self.last_error_message = "Keine ID für den Fall erhalten"
             return self.EBKuSError(REQUEST, RESPONSE)
         fall = Fall(int(fallid))
-        akte = Akte(fall['akte_id'])
-        leistungen = fall['leistungen']
-        leistungen.sort('bgy', 'bgm', 'bgd')
-        leistungsarten = get_codes('fsle')
+        leistung = {
+            'id': Leistung().getNewId(),
+            'bgy': today().year,
+            'bgm': today().month,
+            'bgd': today().day,
+            'ey': '',
+            'em': '',
+            'ed': '',
+            'stz': self.stelle['id'],
+            }
+        return self._process(REQUEST, RESPONSE,
+                             "Neue Leistung eintragen",
+                             "Neue Leistung eintragen für",
+                             fall,
+                             leistung,
+                             'leisteinf',
+                             self.mitarbeiter['id'],
+                             None,
+                             )
         
-        hidden = {'file': 'leisteinf'}
-        leistid = Leistung().getNewId()
-        hiddenid ={'name': 'leistid', 'value': leistid}
-        hiddenid2 ={'name': 'stz', 'value': self.stelle['id']}
-        
-        # Liste der Templates als String
-        
-        res = []
-        res.append(head_normal_t %("Neue Leistung eintragen"))
-        res.append(thleistneu_t %fall)
-        res.append(thleistneu2_t %akte)
-        mksel(res, mitarbeiterliste_t, mitarbeiterliste, 'ben', user)
-        res.append(leistneu_t)
-        mksel(res, codeliste_t, leistungsarten)
-        res.append(leistneubg_t % today())
-        res.append(formhiddenvalues_t % hidden)
-        res.append(formhiddennamevalues_t % hiddenid)
-        res.append(formhiddennamevalues_t % hiddenid2)
-        if leistungen == []:
-            res.append(keineleistungneu_t)
-        else:
-            res.append(thleistungsliste_t)
-            for l in leistungen:
-                res.append(leistungsliste_t % l)
-        res.append(leistungsliste_ende_t)
-        return string.join(res, '')
-        
-        
-class updleist(Request.Request):
+class updleist(_leist):
     """Leistung ändern. (Tabelle: Leistung.)"""
     
     permissions = Request.UPDATE_PERM
     
     def processForm(self, REQUEST, RESPONSE):
-        mitarbeiterliste = self.getMitarbeiterliste()
-        user = self.user
         if self.form.has_key('leistid'):
             id = self.form.get('leistid')
         else:
@@ -73,29 +85,13 @@ class updleist(Request.Request):
             return self.EBKuSError(REQUEST, RESPONSE)
         leistung = Leistung(int(id))
         fall = Fall(leistung['fall_id'])
-        akte = Akte(fall['akte_id'])
-        leistungen = fall['leistungen']
-        leistungen.sort('bgy', 'bgm', 'bgd')
-        leistungsarten = get_codes('fsle')
-        
-        hidden ={'file': 'updleist'}
-        
-        # Liste der Templates als String
-        
-        res = []
-        res.append(head_normal_t % ('Leistung bearbeiten'))
-        res.append(thleistneu_t %fall)
-        res.append(formhiddenvalues_t % hidden)
-        res.append(thleistupd_t %akte)
-        mksel(res, mitarbeiterliste_t, mitarbeiterliste, 'id', leistung['mit_id'])
-        res.append(leistneu_t)
-        mksel(res, codeliste_t, leistungsarten,  'id', leistung['le'])
-        res.append(leistupdbg_t % leistung)
-        if leistungen == []:
-            res.append(keineleistungneu_t)
-        else:
-            res.append(thleistungsliste_t)
-            for l in leistungen:
-                res.append(leistungsliste_t % l)
-        res.append(leistungsliste_ende_t)
-        return string.join(res, '')
+        return self._process(REQUEST, RESPONSE,
+                             "Leistung bearbeiten",
+                             "Leistung bearbeiten von",
+                             fall,
+                             leistung,
+                             'updleist',
+                             leistung['mit_id'],
+                             leistung['le']
+                             )
+
