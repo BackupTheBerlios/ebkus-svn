ab1d1684bd63fc21b5be6d7a8fa91d3f45bc0abf
atms <atms@fe16d31a-7d20-0410-93c1-ede0537ddd7b>
2008-02-19 11:27:39 +0000

    Wdh. Release 4.1

--- ebkus-3/trunk/lib/ebkus/__init__.py
+++ ebkus-3/trunk/lib/ebkus/__init__.py
@@ -2,5 +2,5 @@
 Version = "4.1"
 __authors__  = ["Albrecht Schmiedel <atms@alibi.in-berlin.de>",
                 "Juerg Huber <juerg@pantau.in-berlin.de>"]
-__date__    = "2008-02-02"
+__date__    = "2008-02-19"
 
