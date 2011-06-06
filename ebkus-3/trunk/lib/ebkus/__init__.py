a75094ef1a73a369365e92aead06842cff6cad6c
atms <atms@fe16d31a-7d20-0410-93c1-ede0537ddd7b>
2011-06-06 20:32:59 +0000

    Neue Version 4.4.1

--- ebkus-3/trunk/lib/ebkus/__init__.py
+++ ebkus-3/trunk/lib/ebkus/__init__.py
@@ -1,6 +1,6 @@
 # coding: latin-1
-Version = "4.4"
+Version = "4.4.1"
 __authors__  = ["Albrecht Schmiedel <albrecht.schmiedel@ebkus.org>",
                 "Juerg Huber <juerg@pantau.in-berlin.de>"]
-__date__    = "2011-02-14"
+__date__    = "2011-10-06"
 
