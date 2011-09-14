12105baa75c631b8b8a124ec1bd125bf32bc7771
atms <atms@fe16d31a-7d20-0410-93c1-ede0537ddd7b>
2011-09-14 15:01:49 +0000

    Fehler in diesem Tag

--- ebkus-3/tags/EBKuS-4.4.2/lib/ebkus/__init__.py
+++ ebkus-3/tags/EBKuS-4.4.2/lib/ebkus/__init__.py
@@ -1,6 +1,6 @@
 # coding: latin-1
-Version = "4.4.1"
+Version = "4.4.2"
 __authors__  = ["Albrecht Schmiedel <albrecht.schmiedel@ebkus.org>",
                 "Juerg Huber <juerg@pantau.in-berlin.de>"]
-__date__    = "2011-04-07"
+__date__    = "2011-09-14"
 
